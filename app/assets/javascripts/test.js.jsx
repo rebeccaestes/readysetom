176ad4cc1dcd8cb07a7736e4c1163bb15afa4328
Rebecca Estes <estes.rebecca@gmail.com>
2016-01-10 22:42:44 -0500

    styling

--- app/assets/javascripts/test.js.jsx
+++ app/assets/javascripts/test.js.jsx
@@ -2,28 +2,15 @@ var Test = React.createClass({
 
   render: function() {
     return (
-      <div>
-        Hello from React
-      </div>
+      <h3>
+        The premiere yoga sequence builder on the interwebs
+      </h3>
     );
   }
 });
 
-
-// ReactDOM.render(
-//   <CommentBox url="/api/comments" pollInterval={2000} />,
-//   document.getElementById('content')
-// );
-
-// $(function() {
-//   React.renderComponent(
-//     document.getElementById('content')
-//   );
-// })
-
 var element = React.createElement(Test);
 
-// where to put element the class renders
 $(function() {
   ReactDOM.render(element, document.querySelector('#content'));
 });
